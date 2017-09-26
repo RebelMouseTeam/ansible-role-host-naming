7f94409d76001a72f13d2f8fa35cd8b9c515397f
Andrey Gubarev <mylokin@me.com>
2017-09-26 22:15:58 +0300

    fix style

--- files/host_naming.py
+++ files/host_naming.py
@@ -15,7 +15,9 @@ def get_instances(filters):
 def get_instance(instance_id):
     filters = [{'Name': 'instance-id', 'Values': [instance_id]}]
     instances = get_instances(filters)
-    instances = [i for r in instances['Reservations'] for i in r['Instances'] if i['InstanceId'] == instance_id]
+    instances = [
+        i for r in instances['Reservations']
+        for i in r['Instances'] if i['InstanceId'] == instance_id]
     if instances:
         return instances[0]
 
@@ -28,16 +30,30 @@ def get_tag(instance, tag):
 
 
 def set_tag(instance_id, tag, value):
-    client.create_tags(Resources=[instance_id], Tags=[{'Key': tag, 'Value': value}])
+    tags = [{'Key': tag, 'Value': value}]
+    client.create_tags(Resources=[instance_id], Tags=tags)
+
+
+def set_instance_name(instance_id, group, name_tag, group_tag):
+    pass
 
 
 def main():
     parser = argparse.ArgumentParser(description='Host Naming')
-    parser.add_argument('instance-id', help='EC2 instance id')
+    parser.add_argument('instanceId', help='EC2 instance id')
     parser.add_argument('group', help='Group to use for instance naming')
-    parser.add_argument('-n', '--name-tag', help='Tag where name value should be set("Name" by default)', default='Name')
-    parser.add_argument('-g', '--group-tag', help='Tag where group value is stored("Group" by default)', default='Group')
+    parser.add_argument(
+        '-n',
+        '--nameTag',
+        help='Tag where name value should be set("Name" by default)',
+        default='Name')
+    parser.add_argument(
+        '-g',
+        '--groupTag',
+        help='Tag where group value is stored("Group" by default)',
+        default='Group')
     args = parser.parse_args()
+    set_instance_name(args.instanceId, args.group, args.nameTag, args.groupTag)
 
 
 if __name__ == '__main__':
